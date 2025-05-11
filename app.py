84033ba5a955778dd8d8f856277891e53c91c559
janle-ne <106240409@student.vgu.edu.vn>
2025-05-11 21:12:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ def show():
         if selected_group == "All":
             st.subheader("📊 Top 10 Billionaires")
         else:
-            st.subheader(f"📊 Top Billionaires in {selected_group} Group")
+            st.subheader(f"📊 Top Billionaires in {selected_group} Group")  # Fixed the typo here
 
         st.dataframe(
             top10[["Rank", "Name", "Age", "NetWorth"]].reset_index(drop=True),
